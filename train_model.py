e530673a80b5ecec84ad6c070554405989e796fb
Ross <KAlvYY@163.com>
2024-01-20 09:40:34 +0800

    train process

--- train_model.py
+++ train_model.py
@@ -1,91 +1,86 @@
 # 接下来就开始写这个了
 import os
+import re
 import torch
 import Wide_Deep
 import utility.dataset
+import matplotlib.pyplot as plt
+import numpy as np
 import utility.config
 
-import torch.optim as optim
-
 from tqdm import tqdm
-from tensorboardX import SummaryWriter
 
 
 args = utility.dataset.args
 
-writer = SummaryWriter(logdir='./logdir')
-
-model = Wide_Deep.WideDeep(utility.config.api_range)
+model = Wide_Deep.WideAndDeep()
 model = model.to(utility.config.device)
 
-data_loader = utility.dataset.get_dataloader()
 
 # 定义一个损失函数
 criterion = torch.nn.BCELoss()
 
 # 两个部分的优化方式不同
-wide_optimizer = optim.Adagrad(model.wide.parameters(), lr=args.lr, weight_decay=args.weight_decay)
-deep_optimizer = optim.Adam(model.deep.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+params = model.named_parameters()
+deep_params = []
+wide_params = []
+for name, param in params:
+    if 'wide' in name:
+        wide_params.append(param)
+    else:
+        deep_params.append(param)
+wide_optimizer = torch.optim.Adam(wide_params, lr=args.lr, weight_decay=args.weight_decay)
+deep_optimizer = torch.optim.Adagrad(deep_params, lr=args.lr, weight_decay=args.weight_decay)
 
-fold: str = '4'
+fold: str = re.findall('[0-9]', args.dataset)[0]
 path: str = 'model_wide_deep'
 
 
-def ensure_dir(path: str) -> None:
-    if not os.path.exists(path):
-        os.makedirs(path)
+def ensure_dir(ensure_path: str) -> None:
+    if not os.path.exists(ensure_path):
+        os.makedirs(ensure_path)
+
+
+loss_list = []
 
 
 def train() -> None:
-    train_bar = tqdm(desc='train process...', leave=True, total=args.epoch)
     # 什么时候开始训练呢?
     model.train()
     for i in range(args.epoch):
-        for idx, (train_input, label) in enumerate(data_loader):
-            output = model(train_input)  # output: [batch_size, 1]
-            # 这里有两个损失值，该怎么设计？
-            labels = label.to(utility.config.device)
+        data_loader = utility.dataset.get_dataloader()
+        print('load data completed.')
+        bar = tqdm(data_loader, total=len(data_loader), ascii=True, desc='train')
+        for idx, (datas, labels) in enumerate(bar):
+            # forward
+            outputs = model(datas)
+            labels = labels.to(utility.config.device).float()
+            outputs = outputs.view(-1).float()
+            loss_wide = criterion(outputs, labels)
 
-            output = output.view(-1).float()
-            loss_wide = criterion(output, labels)
-
-            # 优化wide部分
             wide_optimizer.zero_grad()
-            deep_optimizer.zero_grad()
-            loss_wide.backward()
-            deep_optimizer.step()
+            loss_wide.backward(retain_graph=True)
             wide_optimizer.step()
 
-            ###########################################
-            # 优化deep部分, 这两个部分是分开优化的---------> 这里可能最后要用，这里先保存着，如果需要在把上面的改一下
-            # output = model(train_input)
-            # output = output.view(-1).float()
-            # loss_deep = criterion(output, labels)
-            #
-            # deep_optimizer.zero_grad()
-            # loss_deep.backward()
-            # deep_optimizer.step()
-            ###########################################
+            outputs = model(datas)
+            labels = labels.to(utility.config.device).float()
+            outputs = outputs.view(-1).float()
+            loss_deep = criterion(outputs, labels)
 
-            global_step_idx = i * args.train_batch_size + idx + 1
-            # writer.add_scalar(tag='deep loss curve', scalar_value=loss_deep.item(), global_step=global_step_idx)
-            writer.add_scalar(tag='loss curve', scalar_value=loss_wide.item(), global_step=global_step_idx)
+            deep_optimizer.zero_grad()
+            loss_deep.backward()
+            deep_optimizer.step()
 
-            # 记录一下模型的参数
-            for name, parameter in model.named_parameters():
-                writer.add_histogram(name, parameter.data, global_step=global_step_idx)
+            loss_list.append(loss_deep.item())
 
-        # 然后每10个epoch记录模型
-            if idx % 100 == 0:
-                torch.save(wide_optimizer.state_dict(), './' + path + '/model_' + fold + '.pth')
-                torch.save(deep_optimizer.state_dict(), './' + path + '/wide_optimizer_' + fold + '.pth')
-                torch.save(model.state_dict(), './' + path + '/deep_optimizer_' + fold + '.pth')
-        torch.save(wide_optimizer.state_dict(), './' + path + '/model_' + fold + '.pth')
-        torch.save(deep_optimizer.state_dict(), './' + path + '/wide_optimizer_' + fold + '.pth')
-        torch.save(model.state_dict(), './' + path + '/deep_optimizer_' + fold + '.pth')
-
-        train_bar.update()
-    train_bar.close()
+            bar.set_description("epoch:{} idx:{} loss:{:.3f}".format(i, idx, np.mean(loss_list)))
+            if not (idx % 100):
+                torch.save(model.state_dict(), './' + path + '/model_' + fold + '.pth')
+                torch.save(wide_optimizer.state_dict(), './' + path + '/wide_optimizer_' + fold + '.pth')
+                torch.save(deep_optimizer.state_dict(), './' + path + '/deep_optimizer_' + fold + '.pth')
+        torch.save(wide_optimizer.state_dict(), './' + path + '/wide_optimizer_' + fold + '.pth')
+        torch.save(deep_optimizer.state_dict(), './' + path + '/deep_optimizer_' + fold + '.pth')
+        torch.save(model.state_dict(), './' + path + '/model_' + fold + '.pth')
 
 
 if __name__ == '__main__':
@@ -96,3 +91,6 @@ if __name__ == '__main__':
         deep_optimizer.load_state_dict(torch.load('./' + path + '/deep_optimizer_' + fold + '.pth'))
 
     train()
+    plt.figure(figsize=(50, 8))
+    plt.plot(range(len(loss_list)), loss_list)
+    plt.show()
