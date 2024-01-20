e330c415a09554ccdda125cea39e7d4905c62710
Ross <KAlvYY@163.com>
2024-01-20 09:38:58 +0800

    model

--- Wide_Deep.py
+++ Wide_Deep.py
@@ -27,14 +27,16 @@ class WideAndDeep(nn.Module):
     def forward(self, input):
         wide_inputs = [input['encoded_used_api'], input['encoded_candidate_api'],
                        input['cross_product_used_candidate_api']]
-        wide_input = torch.cat(wide_inputs, dim=1)
-        wide_input = torch.tensor(wide_input, dtype=torch.float32)
+        wide_input = torch.cat(wide_inputs, dim=1).float()
+        wide_input = wide_input.to(config.device)
+        # wide_input = torch.tensor(wide_input, dtype=torch.float32)
         wide_output = self.wide(wide_input)
+
         deep_inputs = [input['encoded_used_api'], input['encoded_candidate_api'], input['mashup_description_feature'], input['candidate_api_description_feature']]
-        deep_input = torch.cat(deep_inputs, dim=1)
+        deep_input = torch.cat(deep_inputs, dim=1).float()
         deep_input = deep_input.to(config.device)
-        # print(deep_input.size())
-        deep_input = deep_input.float()
+
         deep_output = self.deep(deep_input)
         output = self.wide_deep(wide_output + deep_output + self.deep_bias)
+
         return output
