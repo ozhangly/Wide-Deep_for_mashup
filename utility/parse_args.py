91d938a4c2045ad61307a4f5fecaca28e7660621
Ross <KAlvYY@163.com>
2024-01-20 09:51:18 +0800

    args parser

--- utility/parse_args.py
+++ utility/parse_args.py
@@ -4,25 +4,23 @@ import argparse
 def arg_parse():
     args = argparse.ArgumentParser()
 
+    # 训练集路径
     args.add_argument('--training_data_path', nargs='?', default='./training data/', type=str)
 
+    # 测试集路径
     args.add_argument('--testing_data_path', nargs='?', default='./testing data/', type=str)
 
-    args.add_argument('--mashup_desc_path', nargs='?', default='./origin dataset/ma_desc_feature/', type=str)
-
+    # api文本描述的路径
     args.add_argument('--api_desc_path', nargs='?', default='./origin dataset/api_desc_feature/', type=str)
 
+    # 输出结果路径
     args.add_argument('--output_path', nargs='?', default='./output/', type=str)
 
-    args.add_argument('--save_weight_path', nargs='?', default='./weight/', type=str)
-
+    # 训练数据集
     args.add_argument('--dataset', nargs='?', default='train_0_torch.json', type=str)
 
+    # 测试数据集
     args.add_argument('--test_dataset', nargs='?', default='testing_0.json', type=str)
-
-    args.add_argument('--recommend_res', nargs='?', default='testing_WD_0.json', type=str)
-
-    args.add_argument('--result', nargs='?', default='result.csv', type=str)
 
     # 还有一些模型和训练的参数
     args.add_argument('--continue_training', nargs='?', default=0, type=int)
@@ -31,7 +29,7 @@ def arg_parse():
 
     args.add_argument('--test_batch_size', nargs='?', default=64, type=int)
 
-    args.add_argument('--epoch', nargs='?', default=100, type=int)
+    args.add_argument('--epoch', nargs='?', default=10, type=int)
 
     args.add_argument('--lr', nargs='?', default=0.01, type=float)
 
