a477641ff28aba9bc857f642c96664afba8bc734
MaxBlack <963137330@qq.com>
2024-03-12 10:10:18 +0800

    fix data type and device error

--- Wide_Deep.py
+++ Wide_Deep.py
@@ -6,31 +6,22 @@ import utility.config as config
 from torch.nn.functional import softmax
 
 
-class ScaledDotProductAttention(nn.Module):
+class AttentionModule(nn.Module):
     def __init__(self):
-        super(ScaledDotProductAttention, self).__init__()
+        super(AttentionModule, self).__init__()
+        self.W_K = nn.Linear(config.api_range, config.args.d_k)     # W_K: [api_range, d_k]
+        self.W_V = nn.Linear(config.api_range, config.args.d_v)     # W_V: [api_range, d_v]
+        self.W_Q = nn.Linear(config.api_range, config.args.d_q)     # W_Q: [api_range, d_q]
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, Q, K, V):                                             # Q: [batch_size, d_q], K: [batch_size, d_k], V: [batch_size, d_v]  d_k == d_q
-        attn_mat = torch.matmul(Q, K.transpose(-1, -2))                     # attn_mat: [batch_size, batch_size]
-        attn_score = softmax(attn_mat/np.sqrt(K.shape[-1]), dim=-1)         # attn_score: [batch_size, batch_size]
-        context = torch.matmul(attn_score, V)                               # context: [batch_size, d_v]
+    def forward(self, input_k, input_v, input_q):   # input_k: [batch_size, api_range]  input_q=input_k  input_v: [batch_size, d_v]
+        K = self.W_K(input_k)               # K: [batch_size, d_k]
+        V = self.W_V(input_v)               # V: [batch_size, d_v]
+        Q = self.W_Q(input_q)               # Q: [batch_size, d_q]    d_k == d_q
 
-        return context
-
-
-class AttentionLayer(nn.Module):
-    def __init__(self):
-        super(AttentionLayer, self).__init__()
-        # 需要三个线性层和一个Softmax
-        self.W_K = nn.Linear(config.api_range, config.args.d_k, bias=False)
-        self.W_V = nn.Linear(config.api_range, config.args.d_v, bias=False)
-        self.W_Q = nn.Linear(config.api_range, config.args.d_q, bias=False)
-
-    def forward(self, input_K, input_V, input_Q):                      # inputs : [batch_size, api_range]
-        K = self.W_K(input_K)                                          # K: [batch_size, d_k]
-        V = self.W_V(input_V)                                          # V: [batch_size, d_v]
-        Q = self.W_Q(input_Q)                                          # Q: [batch_size, d_q]
-        context = ScaledDotProductAttention()(Q, K, V)                 # context: [batch_size, d_v]
+        attn_mat = torch.matmul(Q, K.transpose(-1, -2))             # attn_mat: [batch_size, batch_size]
+        attn_score = self.softmax(attn_mat/np.sqrt(K.shape[-1]))    # attn_score=attn_mat
+        context = torch.matmul(attn_score, V)                       # context: [batch_size, d_v]
 
         return context
 
@@ -38,12 +29,13 @@ class AttentionLayer(nn.Module):
 class WideAndDeep(nn.Module):
     def __init__(self):
         super(WideAndDeep, self).__init__()
+
         self.wide = nn.Sequential(
             nn.Linear(config.api_range + config.api_range + config.api_range * config.api_range, 1),
             nn.Sigmoid()
         )
 
-        self.attention_layer = AttentionLayer()
+        self.attn_layer = AttentionModule()
 
         self.deep = nn.Sequential(
             nn.Linear(config.args.d_v + 1024, 512),
@@ -60,21 +52,26 @@ class WideAndDeep(nn.Module):
     def forward(self, input):
         wide_inputs = [input['encoded_used_api'], input['encoded_candidate_api'],
                        input['cross_product_used_candidate_api']]
-        wide_input = torch.cat(wide_inputs, dim=1).float()                             # wide_input: [batch_size, api_range^2 + 2*api_range]
+        wide_input = torch.cat(wide_inputs, dim=1).float()                        # wide_input: [batch_size, api_range^2 + 2*api_range]
         wide_input = wide_input.to(config.device)
         wide_output = self.wide(wide_input)
 
-        input_k, input_v, input_q = input['encoded_used_api'], \
-                                    input['encoded_used_api'], \
-                                    input['encoded_candidate_api']
+        input_k, input_v, input_q = input['encoded_used_api'], input['encoded_used_api'], input['encoded_candidate_api']
+        input_k = input_k.to(config.device).float()
+        input_v = input_v.to(config.device).float()
+        input_q = input_q.to(config.device).float()
 
-        api_context = self.attention_layer(input_k, input_v, input_q)                  # api_context: [batch_size, d_v]
+        api_context = self.attn_layer(input_k, input_v, input_q)            # input_k=input_q: [batch_size, d_q], input_v: [batch_size, d_q]
 
-        deep_inputs = [input['mashup_description'], api_context, input['candidate_api_description']]
-        deep_input = torch.cat(deep_inputs, dim=1).float()                             # deep_input: [batch_size, dv + 1024]
-        deep_input = deep_input.to(config.device)
+        mashup_description_feature = input['mashup_description_feature']
+        candidate_api_description_feature = input['candidate_api_description_feature']
+        mashup_description_feature = mashup_description_feature.to(config.device).float()
+        candidate_api_description_feature = candidate_api_description_feature.to(config.device).float()
 
+        deep_inputs = [mashup_description_feature, api_context, candidate_api_description_feature]
+        deep_input = torch.cat(deep_inputs, dim=1)                              # deep_input: [batch_size, dv + 1024]
         deep_output = self.deep(deep_input)
+
         output = self.wide_deep(wide_output + deep_output + self.deep_bias)
 
         return output
