f3176e9d7fdff76849cddca158862400fa2e0417
MaxBlack <963137330@qq.com>
2024-03-26 14:01:37 +0800

    transcoding file

--- utility/transcoding.py
+++ utility/transcoding.py
@@ -20,11 +20,11 @@ def encode_api(api, api_range: int = config.api_range) -> np.ndarray:
     encoded_api[api - 1] = 1
     return encoded_api
 
-@jit
-def encode_api_context(api_list, api_range: int = config.api_range) -> np.ndarray:
+
+def encode_api_context(api_list) -> np.ndarray:
     if len(api_list) == 0:
-        return np.zeros(shape=(1, api_range))               # 如果api_list 为空, 初始化一个1 × api_range的二维0数组，目的是为了后面进行拼接
-    return np.array([encode_api(api) for api in api_list])  # 为在data_list中的每一个api初始化一个one-hot向量
+        return np.zeros(shape=(1, config.desc_feature_dim))                          # 如果api_list 为空, 初始化一个1 × 512的二维0数组，目的是为了后面进行拼接
+    return np.array([np.loadtxt('%s%d.txt' % (args.api_desc_path, api)) for api in api_list])      # data_list中的每个api的文本描述特征
 
 @jit
 def cross_product_transformation(encoded_api_list, encoded_api) -> np.ndarray:
